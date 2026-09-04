2f7034882f1f0d11aaa5e1bb5de39d75173cdf70
sh-Enoch <wekesawafula.enoch@gmail.com>
2026-09-04 23:54:02 +0300

    Add Enrollments link to Header and create Enrollments page component

---
+++ frontend/app/(dash)/enrollments/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="space-y-6 flex flex-col">
+      <div className="mx-auto w-fit rounded-xl bg-white px-4 py-2 text-black">
+        <h1 className="text-xl font-semibold leading-tight tracking-wide">
+          Enrollments
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default page;
