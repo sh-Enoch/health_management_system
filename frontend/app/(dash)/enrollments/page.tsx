eb3884edc453bd7ace333876bf786c5da5a7fd5d
sh-Enoch <wekesawafula.enoch@gmail.com>
2026-09-08 23:43:26 +0300

    Update enrollments page message to indicate current content

--- frontend/app/(dash)/enrollments/page.tsx
+++ frontend/app/(dash)/enrollments/page.tsx
@@ -10,7 +10,7 @@ function page() {
       </div>
       <div>
         <p className="text-center text-gray-500">
-          This page is under construction. Please check back later for updates.
+          Thuis will be the enrollments
         </p>
       </div>
     </div>
